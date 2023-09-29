4486024102ee329170ae31086545eb3be9af21bb
Raúl Arieiro <raularieiro20@gmail.com>
2023-09-29 11:59:09 +0100

    Adicionar novas variaveis

--- Semana01/TiposVarios/Program.cs
+++ Semana01/TiposVarios/Program.cs
@@ -7,7 +7,18 @@ namespace TiposVarios
         static void Main(string[] args)
         {
             sbyte u = 56;
+            int i = 777232;
+
+            float f = 666f;
+            double do = 9.99;
+
+            
+
             Console.WriteLine(u);
+            Console.WriteLine(i);
+            Console.WriteLine(f);
+            Console.WriteLine(do);
+            Console.WriteLine()
         }
     }
 }
