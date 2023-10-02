81cb7a09d72c0e7e5119273a1cabdf8a73d59560
Raúl Arieiro <raularieiro20@gmail.com>
2023-10-02 12:16:32 +0100

    Add all

---
+++ Semana02/VariasStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ju = "Judeus nao morreram";
+            Console.WriteLine(ju);
+            string jut = "\t Judeus amam o gas";
+            Console.WriteLine(jut);
+            string jub = "\t Hitler\\Kills -1\\Acistencias 100M\\Death 1 \\\u5350";
+            Console.WriteLine(jub);
+            string judeu;
+        }
+    }
+}
