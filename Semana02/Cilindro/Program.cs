dafea4c8096e3c340898424e055dbfc243f2d82a
Raúl Arieiro <raularieiro20@gmail.com>
2023-10-06 11:25:45 +0100

    Add all

--- Semana02/Cilindro/Program.cs
+++ Semana02/Cilindro/Program.cs
@@ -10,18 +10,18 @@ namespace Cilindro
             double altura;
             double volume;
             double s;
-            double pi = 3.1415926;
 
             Console.WriteLine("Introduza a altura do cilindro ");
             altura = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Introduza o raio do cilindro");
             raio = Convert.ToDouble(Console.ReadLine());
 
-            volume =  pi * (raio * raio) * altura;
-            Console.WriteLine("O valor do volume do cilindro e: ");
+            volume =  Math.PI * Math.Pow(raio,2) * altura;
+            Console.Write("O valor do volume do cilindro e: ");
             Console.WriteLine(volume);
-            Console.WriteLine("/n O volar da superficie e: ");
-            s = 2 * pi * raio * (raio * altura);
+            Console.Write("O volar da superficie e: ");
+            s = 2 * Math.PI * raio * (raio * altura);
+            Console.WriteLine(s);
 
         }
     }
