35de42764a525bddc9ffe0000bfdb36371dc2feb
Raúl Arieiro <raularieiro20@gmail.com>
2023-10-09 12:24:49 +0100

    Usamos o IF  e o Swich

---
+++ Semana02/Skynet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Skynet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            double nr;
+            double n;
+
+            Console.WriteLine("Faça uma pergunta ");
+            str = Console.ReadLine();
+            switch (str)
+            {
+                case "A capital da Alemanha ":
+                    Console.WriteLine("Berlim");
+                    break;
+
+                default:
+                    Console.WriteLine("Sem resposta");
+                    break;
+            }
+            Console.WriteLine("Adicione dois numeros ");
+
+            nr = Convert.ToDouble(Console.ReadLine());
+            n = Convert.ToDouble(Console.ReadLine());
+            if (nr * n > 0)
+            {
+                Console.WriteLine("O resultado e possitivo ");
+            }
+        }
+    }
+}
