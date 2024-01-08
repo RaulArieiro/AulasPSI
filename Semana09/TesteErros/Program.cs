048e262ffd453484f8415e1ae865f9a643e974ca
Raúl Arieiro <raularieiro20@gmail.com>
2024-01-08 13:06:30 +0000

    Começar o exr 6

--- Semana09/TesteErros/Program.cs
+++ Semana09/TesteErros/Program.cs
@@ -6,10 +6,22 @@ namespace TesteErros
     {
         static void Main(string[] args)
         {
+            try{
+
             Console.WriteLine("Insere um numero interio: ");
             int i= Convert.ToInt32(Console.ReadLine());
             Console.WriteLine($"Numero inteiro: {i}");
-            
+
+            }catch(FormatException){
+                //Numero diferente de inteiro
+                Console.WriteLine("Numero diferente de int");
+            }catch(OverflowException){
+                //Numero muito grande
+                Console.WriteLine("Numero grante");
+            }catch(Exception){
+                Console.WriteLine("Alguma coisa ");
+            }
         }
+
     }
 }
