5fb786b89e545fb14671e65d527f68e058639ec0
Raúl Arieiro <raularieiro20@gmail.com>
2024-01-12 23:06:25 +0000

    Commit da semana 9

---
+++ Semana09/Ler01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Ler01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("");
+        }
+    }
+}
